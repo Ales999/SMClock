d5c268d08dfa90665eee8856491d988add5fef86
Alexey Mekhanoshin <367257+Ales999@users.noreply.github.com>
2023-07-09 15:42:35 +0500

    Build MSI after success compile project

--- SMClockSetup/MsiPackage.cs
+++ SMClockSetup/MsiPackage.cs
@@ -1,4 +1,40 @@
-﻿using System;
+﻿/*
+
+    This file is part of SmClock.
+
+    SmClock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SmClock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+  (Этот файл — часть SmClock.
+
+   SmClock - свободная программа: вы можете перераспространять ее и/или
+   изменять ее на условиях Стандартной общественной лицензии GNU в том виде,
+   в каком она была опубликована Фондом свободного программного обеспечения;
+   либо версии 3 лицензии, либо (по вашему выбору) любой более поздней
+   версии.
+
+   SmClock распространяется в надежде, что она будет полезной,
+   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+   общественной лицензии GNU.
+
+   Вы должны были получить копию Стандартной общественной лицензии GNU
+   вместе с этой программой. Если это не так, см.
+   <https://www.gnu.org/licenses/>.)
+
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -53,7 +89,7 @@ namespace SMClockSetup
                     new Dir(@"%ProgramMenu%\OCS\SMClock",
                         new ExeFileShortcut("SMClock", "[INSTALLDIR]SMClock.exe", "")
                         {
-                            IconFile = Directory.GetCurrentDirectory() + @"\APP.ico"
+                            IconFile = Path.Combine(Directory.GetCurrentDirectory(), @"SMClockSetup\APP.ico")
                         },
                         new ExeFileShortcut("Uninstall SMclock", "[System64Folder]msiexec.exe", "/x [ProductCode]")
                     )
@@ -65,8 +101,6 @@ namespace SMClockSetup
                     SourceBaseDir = sourceBaseDir,
                     OutFileName = @"Install_SMClock",
                     Encoding = Encoding.UTF8,
-                    LicenceFile = Directory.GetCurrentDirectory() + @"\EULA.rtf",
-                    OutDir = "..",
                     Codepage = "1251",
                     Language = "ru-ru",
                     UpgradeCode = UpgradeCode,
@@ -80,7 +114,8 @@ namespace SMClockSetup
                     //Description = @"Analog clock with sound notifications", // waiting next release WiX
                     //InstallScope = InstallScope.perUser, // - Not supported by Win4!
                 };
-            managedProject.ControlPanelInfo.ProductIcon = Directory.GetCurrentDirectory() + @"\APP.ico";
+            // ---
+            managedProject.ControlPanelInfo.ProductIcon = Path.Combine(Directory.GetCurrentDirectory(), @"SMClockSetup\APP.ico");
 
             managedProject.ResolveWildCards()
             .FindFile((f) => f.Name.EndsWith("SMClock.exe"))
@@ -89,7 +124,7 @@ namespace SMClockSetup
                 new FileShortcut("SMClock.exe", "INSTALLDIR"),
                 new FileShortcut("SMClock", "%Desktop%")
                 {
-                        IconFile = Directory.GetCurrentDirectory() + @"\APP.ico"
+                        IconFile = Path.Combine( Directory.GetCurrentDirectory(), @"SMClockSetup\APP.ico") // IconFile = Directory.GetCurrentDirectory() + @"\APP.ico"
                 }
             };
 
--- SMClockSetup/Program.cs
+++ SMClockSetup/Program.cs
@@ -1,4 +1,40 @@
-﻿using System;
+﻿/*
+
+    This file is part of SmClock.
+
+    SmClock is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SmClock is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+
+  (Этот файл — часть SmClock.
+
+   SmClock - свободная программа: вы можете перераспространять ее и/или
+   изменять ее на условиях Стандартной общественной лицензии GNU в том виде,
+   в каком она была опубликована Фондом свободного программного обеспечения;
+   либо версии 3 лицензии, либо (по вашему выбору) любой более поздней
+   версии.
+
+   SmClock распространяется в надежде, что она будет полезной,
+   но БЕЗО ВСЯКИХ ГАРАНТИЙ; даже без неявной гарантии ТОВАРНОГО ВИДА
+   или ПРИГОДНОСТИ ДЛЯ ОПРЕДЕЛЕННЫХ ЦЕЛЕЙ. Подробнее см. в Стандартной
+   общественной лицензии GNU.
+
+   Вы должны были получить копию Стандартной общественной лицензии GNU
+   вместе с этой программой. Если это не так, см.
+   <https://www.gnu.org/licenses/>.)
+
+*/
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -9,28 +45,37 @@ using static WixSharp.Nsis.Compressor;
 
 namespace SMClockSetup
 {
+    
+
     internal class Program
     {
         private static void Main(string[] args)
         {
-            Compiler.EmitRelativePaths = false;
+           string currentDir = Directory.GetCurrentDirectory();
+           Compiler.EmitRelativePaths = false;
+            
 
-            Run();
+            Run(currentDir);
 
         }
-        private static void Run()
+        private static void Run(string currentDir)
         {
             Version versionApp = new Version(1, 3, 5, 1);
 
             /// <summary>
             /// Каталог где находится релиз который и будем паковать в инсталятор.
             /// </summary>
-            const string SRootDir = @"..\..\..\src\SMClock\bin\Release";
+            const string SRootDir = @"src\SMClock\bin\Release";
+            const string LicenseFile = @"SMClockSetup\EULA.rtf";
+
+            
+            //const string SRootDir = @"..\..\..\src\SMClock\bin\Release";
+
 
             // Подготовим настройки проекта
             var msiProject = MsiPackage.CreateProject(versionApp, SRootDir);
-            msiProject.OutDir = Directory.GetCurrentDirectory();
-
+            msiProject.OutDir = currentDir;
+            msiProject.LicenceFile = Path.Combine(currentDir, LicenseFile);
 
             // Запуск создания инсталятора.
             msiProject.BuildMsi();
